a4fddb5d5b7feeccfb230ebf95817c2a1bc1d7ab
TusharShandilya <tusharshandilya@live.com>
2020-03-05 09:04:54 +0530

    Styling for forgot password added, Minor webpack clean up

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.js?$/,
+				test: /\.jsx?$/,
 				exclude: [path.resolve(__dirname, 'node_modules')],
 				loader: 'babel-loader',
 			},
@@ -80,6 +80,10 @@ module.exports = {
              "/video": {
                 target: "http://localhost:8238",
                 pathRewrite: {"^/video" : ""}
+            },
+             "/reset-password": {
+                target: "http://localhost:8239",
+                pathRewrite: {"^/reset-password" : ""}
             }
         }
     }
