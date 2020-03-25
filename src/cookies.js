3d41e7c7e50b1585fbbe64aea7cbc681cc19a77f
Sarik Siddiqui <siddiquisarik@gmail.com>
2020-03-26 03:37:26 +0530

    updated cookies

--- src/cookies.js
+++ src/cookies.js
@@ -4,7 +4,7 @@ const getCookie = function(name) {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
             var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
+            // Does this cookie string begin with the name we ?
             if (cookie.substring(0, name.length + 1) == (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
